0abb4d2a52289b5899e6e7fa7720278c518a99f1
Daviuka <davi.cardoso@sesidf.org.br>
2024-08-20 11:08:58 -0300

    Davi

---
+++ Atividades/Atividade02.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+    Declare 2 variáveis, cada uma com um valor, e some os resultados concatenados a um texto.
+    
+    Crie um sistema onde o usuário irá inserir dois valores, guarde cada valor em variáveis, 
+em seguida vocês irão realizar as 4 operações básicas de matemática (+; -; *, /) e mostrar esses 
+resultados na tela, também concatenados a um texto indicativo de cada operação.
+    
+    Crie um sistema que receba o nome do usuário, a idade só podendo aceitar numero inteiro, 
+em seguida mostre o nome e idade do usuário e peça que ele digite 2 números para que sejam somados. 
+Mostrar por último a soma realizada para o usuário.
+
+'''
---
+++ Atividades/atividade01.py
@@ -0,0 +1,37 @@
+# FIXME:Variaveis iniciais
+print("\n")
+print("------------")
+nome = input('Digite o seu nome:')
+idade = int(input('Digite a sua idade:'))
+peso = (input('Digite o seu peso: '))
+altura = (input('Digite a sua altura: '))
+print("------------")
+
+idade = int(idade)
+
+print(f'O nome digitado é: {nome}')
+print(f'A idade digitado é: {idade}')
+print(f'O peso digitado é:{peso}')
+print(f'A altura digitado é:{altura}')
+print("------------")
+print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+    Criar um sistema para receber o nome, idade, peso, altura
+    Converter a idade para receber somente números inteiros
+    Imprimir os tipos de dados
+    Imprimir todas as informações concatenadas usando f string
+'''
