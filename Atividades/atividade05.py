83151730101f45ca3c7b543e267667b681c81a64
Daviuka <davi.cardoso@sesidf.org.br>
2024-08-20 11:28:22 -0300

    Davi

---
+++ Atividades/atividade05.py
@@ -0,0 +1,13 @@
+print("\n")
+print('-----------------')
+num1 = input('Digite o primeiro numero: ')
+num1 = int(num1)
+print('-----------------')
+num2 =  int(input('Digite o segundo numero: '))
+print('-----------------')
+
+# FIXME:Operadoes matemático
+soma = num1 + num2
+
+print(f'O números digitados são: {num1} e {num2}')
+print(f'O resultado da soma foi: {soma}')
